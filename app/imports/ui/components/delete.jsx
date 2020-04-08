1555776ca6c14b386dfab3c56f29387a37de8773
Ray4898 <60079565+Ray4898@users.noreply.github.com>
2020-04-07 16:23:32 -1000

    finished Extra-credit

--- app/imports/ui/components/delete.jsx
+++ app/imports/ui/components/delete.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Contacts } from '../../api/stuff/Contacts';
+import swal from 'sweetalert';
 
 class Delete extends React.Component {
   removeContact(docID) {
-    console.log(`Contact to delete is ${docID}`);
-    Contacts.remove(docID);
+    swal({
+      title: "Are you sure you want to delete this contact?",
+      text: "Once deleted, you will not be able to recover this contact!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+        .then((willDelete) => {
+          if (willDelete) {
+            console.log(`Contact to delete is ${docID}`);
+            Contacts.remove(docID);
+            swal("The contact has been deleted!", {
+              icon: "success",
+            });
+          } else {
+            swal("You canceled the deletion");
+          }
+        });
+
+
   }
 
   render() {
